428ecc1ccdf57c15025dee2e09e1d9a34c05bc83
Ziliang Wang <wangziliang05@gmail.com>
2017-10-30 11:23:21 +0800

    autpep8
    autpep8

--- Inception_v3_LSTM/run_inference.py
+++ Inception_v3_LSTM/run_inference.py
@@ -32,12 +32,16 @@ from inference_utils import vocabulary
 
 FLAGS = tf.flags.FLAGS
 
-tf.flags.DEFINE_string("checkpoint_path", "",
-                       "Model checkpoint file or directory containing a model checkpoint file.")
-tf.flags.DEFINE_string("vocab_file", "", 
+tf.flags.DEFINE_string(
+    "checkpoint_path",
+    "",
+    "Model checkpoint file or directory containing a model checkpoint file.")
+tf.flags.DEFINE_string("vocab_file", "",
                        "Text file containing the vocabulary.")
-tf.flags.DEFINE_string("input_files", "",
-                       "File pattern or comma-separated list of file patterns of image files.")
+tf.flags.DEFINE_string(
+    "input_files",
+    "",
+    "File pattern or comma-separated list of file patterns of image files.")
 tf.flags.DEFINE_string("captions_file", "",
                        "Test captions JSON file.")
 
@@ -45,54 +49,56 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 
 def main(_):
-  # Build the inference graph.
-  g = tf.Graph()
-  with g.as_default():
-    model = inference_wrapper.InferenceWrapper()
-    restore_fn = model.build_graph_from_config(configuration.ModelConfig(),
-                                               FLAGS.checkpoint_path)
-  g.finalize()
+    # Build the inference graph.
+    g = tf.Graph()
+    with g.as_default():
+        model = inference_wrapper.InferenceWrapper()
+        restore_fn = model.build_graph_from_config(configuration.ModelConfig(),
+                                                   FLAGS.checkpoint_path)
+    g.finalize()
 
-  # Create the vocabulary.
-  vocab = vocabulary.Vocabulary(FLAGS.vocab_file)
+    # Create the vocabulary.
+    vocab = vocabulary.Vocabulary(FLAGS.vocab_file)
 
-  filenames = []
-  for file_pattern in FLAGS.input_files.split(","):
-    filenames.extend(tf.gfile.Glob(file_pattern))
-  tf.logging.info("Running caption generation on %d files matching %s",
-                  len(filenames), FLAGS.input_files)
+    filenames = []
+    for file_pattern in FLAGS.input_files.split(","):
+        filenames.extend(tf.gfile.Glob(file_pattern))
+    tf.logging.info("Running caption generation on %d files matching %s",
+                    len(filenames), FLAGS.input_files)
 
-  with tf.Session(graph=g) as sess:
-    # Load the model from checkpoint.
-    restore_fn(sess)
+    with tf.Session(graph=g) as sess:
+        # Load the model from checkpoint.
+        restore_fn(sess)
 
-    # Prepare the caption generator. Here we are implicitly using the default
-    # beam search parameters. See caption_generator.py for a description of the
-    # available beam search parameters.
-    generator = caption_generator.CaptionGenerator(model, vocab)
-    image_id_caption = []
-    for filename in filenames:
-      with tf.gfile.GFile(filename, "rb") as f:
-        image = f.read()
-      captions = generator.beam_search(sess, image)
-      print("Captions for image %s:" % os.path.basename(filename))
-      for i, caption in enumerate(captions):
-        # Ignore begin and end words.
-        sentence = [vocab.id_to_word(w) for w in caption.sentence[1:-1]]
-        sentence = "".join(sentence)
-        print("  %d) %s (p=%f)" % (i, sentence, math.exp(caption.logprob)))
-        if not i:
-          image_id_caption.append(    
-            {
-              "image_id": filename.split('/')[-1],
-              "caption": sentence
-            },)
-    image_id_caption = json.dumps(image_id_caption).encode('utf-8')
-    data = json.loads(image_id_caption)
-    with open(FLAGS.captions_file, 'w') as f:
-      json.dump(data, f)
-    print("Saving captions file to path %s" % FLAGS.captions_file)
+        # Prepare the caption generator. Here we are implicitly using the default
+        # beam search parameters. See caption_generator.py for a description of the
+        # available beam search parameters.
+        generator = caption_generator.CaptionGenerator(model, vocab)
+        image_id_caption = []
+        for filename in filenames:
+            with tf.gfile.GFile(filename, "rb") as f:
+                image = f.read()
+            captions = generator.beam_search(sess, image)
+            print("Captions for image %s:" % os.path.basename(filename))
+            for i, caption in enumerate(captions):
+                # Ignore begin and end words.
+                sentence = [vocab.id_to_word(w)
+                            for w in caption.sentence[1:-1]]
+                sentence = "".join(sentence)
+                print("  %d) %s (p=%f)" %
+                      (i, sentence, math.exp(caption.logprob)))
+                if not i:
+                    image_id_caption.append(
+                        {
+                            "image_id": filename.split('/')[-1],
+                            "caption": sentence
+                        },)
+        image_id_caption = json.dumps(image_id_caption).encode('utf-8')
+        data = json.loads(image_id_caption)
+        with open(FLAGS.captions_file, 'w') as f:
+            json.dump(data, f)
+        print("Saving captions file to path %s" % FLAGS.captions_file)
 
 
 if __name__ == "__main__":
-  tf.app.run()
+    tf.app.run()
--- Inception_v3_LSTM/train.py
+++ Inception_v3_LSTM/train.py
@@ -34,7 +34,10 @@ tf.flags.DEFINE_string("train_dir", "",
                        "Directory for saving and loading model checkpoints.")
 tf.flags.DEFINE_boolean("train_inception", False,
                         "Whether to train inception submodel variables.")
-tf.flags.DEFINE_integer("number_of_steps", 1000000, "Number of training steps.")
+tf.flags.DEFINE_integer(
+    "number_of_steps",
+    1000000,
+    "Number of training steps.")
 tf.flags.DEFINE_integer("log_every_n_steps", 1,
                         "Frequency at which loss and global step are logged.")
 
@@ -42,73 +45,76 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 
 def main(unused_argv):
-  assert FLAGS.input_file_pattern, "--input_file_pattern is required"
-  assert FLAGS.train_dir, "--train_dir is required"
+    assert FLAGS.input_file_pattern, "--input_file_pattern is required"
+    assert FLAGS.train_dir, "--train_dir is required"
 
-  model_config = configuration.ModelConfig()
-  model_config.input_file_pattern = FLAGS.input_file_pattern
-  model_config.inception_checkpoint_file = FLAGS.inception_checkpoint_file
-  training_config = configuration.TrainingConfig()
+    model_config = configuration.ModelConfig()
+    model_config.input_file_pattern = FLAGS.input_file_pattern
+    model_config.inception_checkpoint_file = FLAGS.inception_checkpoint_file
+    training_config = configuration.TrainingConfig()
 
-  # Create training directory.
-  train_dir = FLAGS.train_dir
-  if not tf.gfile.IsDirectory(train_dir):
-    tf.logging.info("Creating training directory: %s", train_dir)
-    tf.gfile.MakeDirs(train_dir)
+    # Create training directory.
+    train_dir = FLAGS.train_dir
+    if not tf.gfile.IsDirectory(train_dir):
+        tf.logging.info("Creating training directory: %s", train_dir)
+        tf.gfile.MakeDirs(train_dir)
 
-  # Build the TensorFlow graph.
-  g = tf.Graph()
-  with g.as_default():
-    # Build the model.
-    model = show_and_tell_model.ShowAndTellModel(
-        model_config, mode="train", train_inception=FLAGS.train_inception)
-    model.build()
+    # Build the TensorFlow graph.
+    g = tf.Graph()
+    with g.as_default():
+        # Build the model.
+        model = show_and_tell_model.ShowAndTellModel(
+            model_config, mode="train", train_inception=FLAGS.train_inception)
+        model.build()
 
-    # Set up the learning rate.
-    learning_rate_decay_fn = None
-    if FLAGS.train_inception:
-      learning_rate = tf.constant(training_config.train_inception_learning_rate)
-    else:
-      learning_rate = tf.constant(training_config.initial_learning_rate)
-      if training_config.learning_rate_decay_factor > 0:
-        num_batches_per_epoch = (training_config.num_examples_per_epoch /
-                                 model_config.batch_size)
-        decay_steps = int(num_batches_per_epoch *
-                          training_config.num_epochs_per_decay)
+        # Set up the learning rate.
+        learning_rate_decay_fn = None
+        if FLAGS.train_inception:
+            learning_rate = tf.constant(
+                training_config.train_inception_learning_rate)
+        else:
+            learning_rate = tf.constant(training_config.initial_learning_rate)
+            if training_config.learning_rate_decay_factor > 0:
+                num_batches_per_epoch = (
+                    training_config.num_examples_per_epoch /
+                    model_config.batch_size)
+                decay_steps = int(num_batches_per_epoch *
+                                  training_config.num_epochs_per_decay)
 
-        def _learning_rate_decay_fn(learning_rate, global_step):
-          return tf.train.exponential_decay(
-              learning_rate,
-              global_step,
-              decay_steps=decay_steps,
-              decay_rate=training_config.learning_rate_decay_factor,
-              staircase=True)
+                def _learning_rate_decay_fn(learning_rate, global_step):
+                    return tf.train.exponential_decay(
+                        learning_rate,
+                        global_step,
+                        decay_steps=decay_steps,
+                        decay_rate=training_config.learning_rate_decay_factor,
+                        staircase=True)
 
-        learning_rate_decay_fn = _learning_rate_decay_fn
+                learning_rate_decay_fn = _learning_rate_decay_fn
 
-    # Set up the training ops.
-    train_op = tf.contrib.layers.optimize_loss(
-        loss=model.total_loss,
+        # Set up the training ops.
+        train_op = tf.contrib.layers.optimize_loss(
+            loss=model.total_loss,
+            global_step=model.global_step,
+            learning_rate=learning_rate,
+            optimizer=training_config.optimizer,
+            clip_gradients=training_config.clip_gradients,
+            learning_rate_decay_fn=learning_rate_decay_fn)
+
+        # Set up the Saver for saving and restoring model checkpoints.
+        saver = tf.train.Saver(
+            max_to_keep=training_config.max_checkpoints_to_keep)
+
+    # Run training.
+    tf.contrib.slim.learning.train(
+        train_op,
+        train_dir,
+        log_every_n_steps=FLAGS.log_every_n_steps,
+        graph=g,
         global_step=model.global_step,
-        learning_rate=learning_rate,
-        optimizer=training_config.optimizer,
-        clip_gradients=training_config.clip_gradients,
-        learning_rate_decay_fn=learning_rate_decay_fn)
-
-    # Set up the Saver for saving and restoring model checkpoints.
-    saver = tf.train.Saver(max_to_keep=training_config.max_checkpoints_to_keep)
-
-  # Run training.
-  tf.contrib.slim.learning.train(
-      train_op,
-      train_dir,
-      log_every_n_steps=FLAGS.log_every_n_steps,
-      graph=g,
-      global_step=model.global_step,
-      number_of_steps=FLAGS.number_of_steps,
-      init_fn=model.init_fn,
-      saver=saver)
+        number_of_steps=FLAGS.number_of_steps,
+        init_fn=model.init_fn,
+        saver=saver)
 
 
 if __name__ == "__main__":
-  tf.app.run()
+    tf.app.run()
